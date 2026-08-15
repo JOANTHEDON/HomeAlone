4ed3ef7e31399d74878c503944e09fc97e74637a
Mosha12 <rb121202@gmail.com>
2026-08-16 00:30:29 +0530

    added navmesh for the ghost

--- Assets/Scripts/GhostController.cs
+++ Assets/Scripts/GhostController.cs
@@ -1,26 +1,33 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GhostController : MonoBehaviour
 {
-    [SerializeField] private float _ghostSpeed = 5f;
+    //[SerializeField] private float _ghostSpeed = 5f;
     private Transform _target;
     [SerializeField]private Vector2 _offset;
+    private NavMeshAgent _navmeshAgent;
 
     private bool _shouldMove = false;
     
     public bool ShouldMove => _shouldMove;
     private void Start()
     {
+        _navmeshAgent = GetComponent<NavMeshAgent>();
+        _navmeshAgent.updateRotation = false;
+        _navmeshAgent.updateUpAxis = false;
         _target = GameObject.FindGameObjectWithTag("Door").transform; 
+        
     }
 
     private void Update()
     {
         if(_target == null ) return;
-        Vector2 currentPos = transform.position;
+        // Vector2 currentPos = transform.position;
         Vector2 targetPos = (Vector2)_target.position + _offset;
-        transform.position = Vector2.MoveTowards(currentPos,targetPos, _ghostSpeed * Time.deltaTime);
+        // transform.position = Vector2.MoveTowards(currentPos,targetPos, _ghostSpeed * Time.deltaTime);
+        _navmeshAgent.SetDestination(targetPos);
 
     }
 
